242179a417cdceba093e57193861706751878273
Brady <brady.withers@outlook.com>
2020-02-28 11:50:22 -0600

    changing naming scheme

--- assets/js/store.js
+++ assets/js/store.js
@@ -5,10 +5,10 @@ $( document ).ready(function() {
 	//function to change image on hover
 $('.hat1').hover(function() {
 	//on hover image changes to the image below
-  $(this).attr('src', '../assets/images/store/hat-test-detail.png');
+  $(this).attr('src', '../assets/images/store/hat-1-test-detail.png');
 }, function() {
 	//after mouse leave the image changes to image below 
-  $(this).attr('src', '../assets/images/store/hat-test.png');
+  $(this).attr('src', '../assets/images/store/hat-1-test.png');
 });
 
 	//function to change image on hover
