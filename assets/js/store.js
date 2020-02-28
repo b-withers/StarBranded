f15acb329e5fbdc77dfa2d5f93a0f381fc18d316
Brady <brady.withers@outlook.com>
2020-02-28 11:35:43 -0600

    working 2nd image

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,11 +1,24 @@
 $( document ).ready(function() {
 	console.log( "store page ready" );
 
+
+	//function to change image on hover
 $('.hat1').hover(function() {
+	//on hover image changes to the image below
   $(this).attr('src', '../assets/images/store/hat-test-detail.png');
 }, function() {
+	//after mouse leave the image changes to image below 
   $(this).attr('src', '../assets/images/store/hat-test.png');
 });
+
+	//function to change image on hover
+	$('.hat2').hover(function() {
+		//on hover image changes to the image below
+		$(this).attr('src', '../assets/images/store/hat-2-test-detail.png');
+	}, function() {
+		//after mouse leave the image changes to image below 
+		$(this).attr('src', '../assets/images/store/hat-2-test.png');
+	});
 
 
 });
